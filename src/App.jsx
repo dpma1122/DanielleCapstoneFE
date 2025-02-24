6cafc2d4160f5c1aaa5c254157c3fb7a47808399
dpma1122 <dpma1122@yahoo.com>
2025-02-24 10:37:12 -0500

    added all imports for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
 import Home from './pages/Home'; 
 import JournalEntry from './pages/JournalEntry'; 
-
+import MindfulMoments from './pages/MindfulMoments'
+import ReflectionHub from './pages/ReflectionHub'
 function App() {
   return (<>
   
