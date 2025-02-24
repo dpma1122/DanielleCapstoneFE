a7e7811fdb03b86e53ffa4bec1966a5670e35141
dpma1122 <dpma1122@yahoo.com>
2025-02-24 10:41:07 -0500

    added self closing route

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import Home from './pages/Home';
 import JournalEntry from './pages/JournalEntry'; 
 import MindfulMoments from './pages/MindfulMoments'
 import ReflectionHub from './pages/ReflectionHub'
+
+
+
 function App() {
   return (<>
-  
+    <Routes>
+      <Route/>
+      <Route/>
+      <Route/>
+      <Route/>
+      
+
+  </Routes>
   
   </>
  
