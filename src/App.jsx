8c5ce2ea1e831acb17aa8de4c325c1eeab366e39
dpma1122 <dpma1122@yahoo.com>
2025-02-24 11:41:40 -0500

    added links to pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import Home from './pages/Home';
 import JournalEntry from './pages/JournalEntry'; 
 import MindfulMoments from './pages/MindfulMoments';
 import ReflectionHub from './pages/ReflectionHub';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom';
 
 
 function App() {
   return (
     <>
+      <nav>
+        <Link to={'/'}>Home</Link>| |
+        <Link to={'/create'}>JournalEntry</Link> | |
+        <Link to={'/update'}>ReflectionHub</Link> | |
+        <Link to= {'/mindfulMoments'}>MindfulMoments</Link> 
+      </nav>
+
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/create' element={<JournalEntry /> } />
