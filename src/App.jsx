e3fa81e773882bc152451adeed757502c01bb0ea
dpma1122 <dpma1122@yahoo.com>
2025-02-24 06:45:31 -0500

    added front end boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { StrictMode } from 'react'; 
 import { createRoot } from 'react-dom/client'; 
 import { BrowserRouter } from 'react-router-dom'; 
-import './index.css'
+import './index.css'; 
+import App from './App.jsx'
 
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
+  </StrictMode>
+)
 
 export default App
