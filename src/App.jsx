4bf92725a4ad41ceae2443c04438c3ac5639d745
dpma1122 <dpma1122@yahoo.com>
2025-02-24 10:15:24 -0500

    added a fucntion to see if something renders on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { StrictMode } from 'react'; 
-import { createRoot } from 'react-dom/client'; 
-import { BrowserRouter } from 'react-router-dom'; 
-import './index.css'; 
-import App from './App.jsx'
+import './App.css'
 
 
+function App() {
+  return (
+    <> <h1> is it working</h1></>
+  )
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
-  </StrictMode>
-)
 
 export default App
